173e7312c70927a3e8dc92e84fa900a399ef2908
Alvin Justine <130334387+viseven27@users.noreply.github.com>
2025-07-23 20:19:30 +0700

    Initial Commit

---
+++ Kulturkita/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kulturkita
+//
+//  Created by Alvin Justine on 23/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kulturkita/KulturkitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KulturkitaApp.swift
+//  Kulturkita
+//
+//  Created by Alvin Justine on 23/07/25.
+//
+
+import SwiftUI
+
+@main
+struct KulturkitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
