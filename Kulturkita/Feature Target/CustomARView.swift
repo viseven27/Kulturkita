8716ec103162e16132309b8331066ff69e377879
Alvin Justine <130334387+viseven27@users.noreply.github.com>
2025-07-26 03:53:17 +0700

    Second commit
    Minimalist POC UI done
    To be added: Anchor and Boat 3D

---
+++ Kulturkita/Feature Target/CustomARView.swift
@@ -0,0 +1,26 @@
+//
+//  CustomARView.swift
+//  Kulturkita
+//
+//  Created by Alvin Justine on 24/07/25.
+//
+
+//import ARKit
+//import RealityKit
+//import SwiftUI
+//
+//class CustomARView: ARView {
+//    required init(frame frameRect: CGRect) {
+//        super.init(frame: frameRect)
+//    }
+//    
+//    dynamic required init?(coder decoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+//    convenience init() {
+//        self.init(frame: UISCreen.main.bounds)
+//    }
+//}
+
+
--- Kulturkita/View/ContentView.swift
+++ Kulturkita/View/ContentView.swift
@@ -9,42 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            //1. Ruang kosong di atas
-            Spacer()
-            //2. Judul, logo, dan tombol
+        NavigationView {
             VStack {
-                Text("Kulturkita")
-                Image("KulturkitaLogo")
-                Button("Click Me") {
-                    // Action here
-                    print("Button was tapped")
-                }
-                .foregroundColor(.brown)
-                .background(Color.yellow)
-            }
-            Spacer()
-            //3.
-            HStack {
+                //1. Ruang kosong di atas
+                Spacer()
+                //2. Judul, logo, dan tombol
                 VStack {
-                    Button("Stamp"){
-                        print(" ")
+                    Text("Kulturkita")
+                    Image("KulturkitaLogo")
+                    NavigationLink(destination: ARContentView()){
+                        Text("Click Me")
+                            .padding(24)
+                            .background(Color.yellow)
+                            .foregroundColor(.brown)
+                            .cornerRadius(40)
                     }
-                    Text("Stempel")
                 }
                 Spacer()
-                
-                VStack{
-                    Button("Map"){
-                        print("")
+                //3.
+                HStack {
+                    VStack {
+                        Button("Stamp"){
+                            print(" ")
+                        }
+                        Text("Stempel")
                     }
-                    Text("Peta")
+                    Spacer()
+                    
+                    VStack{
+                        Button("Map"){
+                            print("")
+                        }
+                        Text("Peta")
+                    }
                 }
+                
             }
-            
+            .padding()
+            .background(Color.yellow.opacity(0.2))
+            .ignoresSafeArea()
         }
-        .padding()
     }
+    
 }
 
 #Preview {
